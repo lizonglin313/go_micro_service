5c17b263957f4995a4bd650d4b6b217e283b4fd6
lizonglin_TNcarrot <lizonglin313@outlook.com>
2021-11-29 15:58:35 +0800

    feat: native gateway

--- gateway/gateway/main.go
+++ gateway/gateway/main.go
@@ -1,17 +1,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/go-kit/kit/log"
 	"github.com/hashicorp/consul/api"
 	"math/rand"
 	"net/http"
 	"net/http/httputil"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 )
 
 func main() {
+	// 创建环境变量
+	var (
+		consulHost = flag.String("consul.host", "127.0.0.1", "consul server ip address")
+		consulPort = flag.String("consul.port", "8500", "consul server port")
+	)
 
+	// 创建日志组件
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	// 创建consul api client
+	consulConfig := api.DefaultConfig()
+	consulConfig.Address = "http://" + *consulHost + ":" + *consulPort
+	consulClient, err := api.NewClient(consulConfig)
+	if err != nil {
+		logger.Log("err", err)
+		os.Exit(1)
+	}
+
+	// 创建反向代理
+	proxy := NewReverseProxy(consulClient, logger)
+
+	errchan := make(chan error)
+	go func() { // 监听终端信号
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errchan <- fmt.Errorf("%s", <-c)
+	}()
+
+	// 监听请求
+	go func() {
+		logger.Log("transport", "HTTP", "addr", "9123")
+		errchan <- http.ListenAndServe(":9123", proxy)
+	}()
+
+	// 等待结束记录log
+	logger.Log("exit", <-errchan)
 }
 
 // NewReverseProxy 创建反向代理.
--- gateway/go.mod
+++ gateway/go.mod
@@ -4,5 +4,8 @@ go 1.16
 
 require (
 	github.com/go-kit/kit v0.12.0
+	github.com/gorilla/mux v1.6.2
 	github.com/hashicorp/consul/api v1.11.0
+	github.com/pborman/uuid v1.2.1
+	github.com/prometheus/client_golang v1.11.0
 )
